533cac81855e73ec05cba8e9e0788a06bdd673f8
Antonij <adzajkoski@gmail.com>
2021-11-01 22:08:41 +0100

    10th class commit

--- React/09-2021/class-10/src/App.js
+++ React/09-2021/class-10/src/App.js
@@ -1,58 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+// import * as AOS from 'aos'
+import { useTransition } from 'react-spring';
 
 function App() {
+  const [triggerRefresh, setTriggerRefresh] = useState(false);
+  useEffect(() => {
+    if (triggerRefresh === true) {
+      console.log('Refresh Triggered!');
+    }
+
+    return () => setTriggerRefresh(false);
+  }, [triggerRefresh]);
+
+  const trigger = () => {
+    setTriggerRefresh(true);
+  };
+  const items = [1, 2, 3];
+  // AOS.init();
+  /* eslint-disable */
+  const transition = useTransition(items, {
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <button onClick={trigger}>Trigger refresh</button>
     </div>
   );
 }
 
 export default App;
+
+// Model.find().where('age').gt(3);
+
+// SELECT * FROM models WHERE age == 3. TypeORM Sequelize
