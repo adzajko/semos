742de8bc2012e721987110950a7bf2f12af6a9ed
Antonij <adzajkoski@gmail.com>
2021-11-15 20:49:19 +0100

    Init setup for class 03

---
+++ Node/11-2021/class-03/routes/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+// router.get('/home', async (req, res, next) => {
+//   return res.status(200).json('Welcome Home (Sanitarium)');
+// });
+
+// router.get('/home2', async (req, res, next) => {});
+
+// router.post('/home', async (req, res, next) => {
+//   const requestBody = req.body;
+//   console.log(requestBody);
+//   return res.status(200).json(requestBody);
+// });
+
+// async (req, res, next) => {} -- This is the function used to handle ANY incoming HTTP request.
+
+/**
+ * POST - C (create)
+ * GET - R (read)
+ * PUT - U (update)
+ * DELETE - D (delete)
+ */
+
+module.exports = router;
