4d7a91d59cc2012ea1600175ed9b97aadd4b1d3f
Antonij <adzajkoski@gmail.com>
2021-12-09 17:34:05 +0100

    Added homework

--- Node/11-2021/class-09/handlers/products.js
+++ Node/11-2021/class-09/handlers/products.js
@@ -2,17 +2,27 @@ const Product = require('../core/Product');
 
 const getAllProducts = async (req, res, next) => {
   try {
+    // Aggregation replaces the .find() method.
     const allProducts = await Product.aggregate()
+      /**
+       * An aggregation is built-up using stages.
+       * In every stage, the results of the previous stage are passed in.
+       * Important stages to note are the group(), match(), lookup() and project()
+       * .sort(), limit() and skip() function the same way as in .find()
+       */
       .match({
+        // Match acts as a filter.
         isInStock: true,
       })
       .lookup({
+        // Lookup enables to see the contents of another collection, acting as a JOIN statement.
         from: 'locations',
         localField: 'locationName',
         foreignField: 'locationName',
         as: 'fullLocationDetails',
       })
       .project({
+        // Project allows to change the objects within the array. Remember to use the $ sign to denote variable names.
         imeNaProduktot: '$productName',
         cenaNaProduktot: '$productPrice',
         voZalihaE: '$isInStock',
