db19b568d7773852ef4f1d4d2803015bbc721598
Antonij <adzajkoski@gmail.com>
2022-02-10 22:09:24 +0100

    MongoDB integrated

--- Node/01-2022/class-08/handlers/jadenja.js
+++ Node/01-2022/class-08/handlers/jadenja.js
@@ -23,9 +23,32 @@ const createNewJadenje = async (req, res, next) => {
   }
 };
 
-const updateJadenje = async (req, res, next) => {};
+const updateJadenje = async (req, res, next) => {
+  const _id = req.params._id;
+  const { name, cuisine } = req.body;
 
-const deleteJadenje = async (req, res, next) => {};
+  if (!name || !cuisine) {
+    return res.status(400).json('Missing data!');
+  }
+
+  try {
+    await Jadenje.findByIdAndUpdate(_id, { name, cuisine });
+    return res.status(200).json('Updated');
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+const deleteJadenje = async (req, res, next) => {
+  const _id = req.params._id;
+
+  try {
+    await Jadenje.findByIdAndDelete(_id);
+    return res.status(200).json('Deleted');
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
 
 module.exports = {
   getAllJadenja,
--- Node/01-2022/class-08/routes/index.js
+++ Node/01-2022/class-08/routes/index.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const apiSpecs = require('../api-specs.json');
-const { getAllJadenja, createNewJadenje } = require('../handlers/jadenja');
+const {
+  getAllJadenja,
+  createNewJadenje,
+  updateJadenje,
+  deleteJadenje,
+} = require('../handlers/jadenja');
 
 const apiPrefix = process.env.API_PREFIX;
 
 router.get(apiPrefix + '/jadenja', getAllJadenja);
 router.post(apiPrefix + '/jadenja', createNewJadenje);
+router.put(apiPrefix + '/jadenja/:_id', updateJadenje);
+router.delete(apiPrefix + '/jadenja/:_id', deleteJadenje);
 
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(apiSpecs));
